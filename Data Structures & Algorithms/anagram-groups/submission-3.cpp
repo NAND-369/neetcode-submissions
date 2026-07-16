8829d22ae648b52bb5a4fbbfc298bbee926c8ebd
NAND-369 <122550676+NAND-369@users.noreply.github.com>
2026-07-17 00:54:06 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string,vector<string>>ans;
+    for(string s:strs){
+        string sorted=s;
+        sort(sorted.begin(),sorted.end());
+        ans[sorted].push_back(s);
+    }
+    vector<vector<string>>result;
+    for(auto&it:ans){
+       result.push_back(it.second);
+    }
+    return result;
+    }
+};
